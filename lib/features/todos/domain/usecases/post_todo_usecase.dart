1ca1ce26b7127f6f043dd46bca2e6d83bd5d5b4c
raihansyahrin <raihansyahrin@gmail.com>
2025-08-24 02:14:33 +0700

    refactor: change void to todos entity

--- lib/features/todos/domain/usecases/post_todo_usecase.dart
+++ lib/features/todos/domain/usecases/post_todo_usecase.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:test_bfi/features/todos/domain/entities/todos_entity.dart';
 import 'package:test_bfi/features/todos/domain/repositories/todos_repository.dart';
 
 class PostTodoUsecase {
@@ -6,7 +7,7 @@ class PostTodoUsecase {
 
   PostTodoUsecase({required this.todosRepository});
 
-  Future<Either<String, void>> call(String title) async {
+  Future<Either<String, TodosEntity>> call(String title) async {
     return await todosRepository.postTodo(title);
   }
 }
